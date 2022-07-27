5995677a648d9abbd208ca88df0b72aad4aa0620
Remisaurus <104589165+Remisaurus@users.noreply.github.com>
2022-07-27 15:37:13 -0400

    27-07-22

---
+++ comments/main.py
@@ -0,0 +1,19 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+#Start this assignment with Wincpy.
+#Write some code in main.py. You're completely free in what you write.
+#Clarify what you did with two single-line comments, two end-of-line comments and two multiline comments.
+#Check your solution with wincpy check
+"""
+So.. this is a multi-line comment.
+"""
+# a single line comment
+print('use a # to initiate a single- or end-line comment') #end-line
+print('use three "to start/end a multi-line comment') # all is clear
+# nothing to add
+"""
+    Could be a docstring
+    """
