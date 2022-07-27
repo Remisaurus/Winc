7e4ff93a85f298a796b94c152af8aae10da1c1f2
Remisaurus <104589165+Remisaurus@users.noreply.github.com>
2022-07-27 13:54:39 -0400

    27-07-22

---
+++ print/main.py
@@ -0,0 +1,6 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print('print(), hmm, if a printer can print itself the human race will be done for after it starts to think')
