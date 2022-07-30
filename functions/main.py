d49d535f4503ee684410e37296618bc74e381e8f
Remisaurus <104589165+Remisaurus@users.noreply.github.com>
2022-07-30 11:35:02 -0400

    30-07-22

---
+++ functions/main.py
@@ -0,0 +1,24 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+
+
+def greet(name):
+    print('Hello, '+name+'!')
+    return('Hello, '+name+'!')
+
+def add(a,b,c):
+    print((a+b+c))
+    return(a+b+c)
+
+def positive(number):
+    print (number>0)
+    return (number>0)
+
+def negative(number):
+     print (number<0)
+     return (number<0)
+
+greet('Bob')
