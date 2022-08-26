3a99d6c94d23baca846f4a3e8fd2f485582fe592
Remi <remi.meijer@gmail.com>
2022-08-26 12:46:52 -0400

    26-08-2022

--- refactoring/main.py
+++ refactoring/main.py
@@ -1,53 +1,100 @@
 __winc_id__ = "9920545368b24a06babf1b57cee44171"
 __human_name__ = "refactoring"
 
-alice_name = "Alice Aliceville"
-alice_profession = "electrician"
-bob_name = "Bob Bobsville"
-bob_profession = "painter"
-craig_name = "Craig Craigsville"
-craig_profession = "plumber"
+# start of classes
 
-alfred_name = "Alfred Alfredson"
-alfred_address = "Alfredslane 123"
-alfred_needs = ["painter", "plumber"]
-bert_name = "Bert Bertson"
-bert_address = "Bertslane 231"
-bert_needs = ["plumber"]
-candice_name = "Candice Candicedottir"
-candice_address = "Candicelane 312"
-candice_needs = ["electrician", "painter"]
+specialist_list = None
+homeowner_list = None
 
-alfred_contracts = []
-for need in alfred_needs:
-    if need == alice_profession:
-        alfred_contracts.append(alice_name)
-    elif need == bob_profession:
-        alfred_contracts.append(bob_name)
-    elif need == craig_profession:
-        alfred_contracts.append(craig_name)
+class specialist():
+    def __init__(self, selfname, name, profession, hometown, price_per_hour):
+        self.selfname = selfname
+        self.name = name
+        self.profession = profession
+        self.hometown = hometown
+        self.price_per_hour = price_per_hour
+        global specialist_list
+        if specialist_list == None:
+            specialist_list = []
+            specialist_list.append(self.selfname)
+        else:
+            specialist_list.append(self.selfname)
+        
+    def identify(self):
+        print(f'I am {self.name}')
+        print(f'I am from {self.hometown}')
+        print(f'I am a specialist that works as {self.profession}')
+        print(f'My price per hour would be {self.price_per_hour}')
+        print(f'you can access me with : {self.selfname}')
+        
+class homeowner():
+    def __init__(self, selfname, name, needs, adress, hometown):
+        self.selfname = selfname
+        self.name = name
+        self.needs = needs
+        self.adress = adress
+        self.hometown = hometown
+        self.contract_possibility_list = []
+        global homeowner_list
+        if homeowner_list == None:
+            homeowner_list = []
+            homeowner_list.append(self.selfname)
+        else:
+            homeowner_list.append(self.selfname)
+        
+    def identify(self):
+       print(f'I am {self.name}')
+       print(f'I live on {self.adress} in {self.hometown}')
+       print(f'I am in need of {self.needs}')
+       print(f'you can access me with : {self.selfname}')
+       print(f'My possible matches with specialist(s) are :{self.contract_possibility_list}')
+       
+# end of classes
+# start of data addition
 
-bert_contracts = []
-for need in bert_needs:
-    if need == alice_profession:
-        bert_contracts.append(alice_name)
-    elif need == bob_profession:
-        bert_contracts.append(bob_name)
-    elif need == craig_profession:
-        bert_contracts.append(craig_name)
+# add data in this form:
+# for specialists: unique_shortname = specialist(unique_shortname, name, profession, hometown, price_per_hour)
+# for homeowners: unique_shortname = homeowner(unique_shortname, name, needs, adress, hometown)
+# if the specific data is unknown, '-' must be used
+# typeo's are not allowed! double check if neccessary! also all in strings ('' around word)!
+       
+alice = specialist('alice','Alice Aliceville','electrician','-', '-')
+bob = specialist('bob','Bob Bobsville','painter','-', '-')
+craig = specialist('craig','Craig Craigsville','plumber','-', '-')
+alice2 = specialist('alice2', 'alice the second', 'electrician','somewhere else','-15')
 
-candice_contracts = []
-for need in candice_needs:
-    if need == alice_profession:
-        candice_contracts.append(alice_name)
-    elif need == bob_profession:
-        candice_contracts.append(bob_name)
-    elif need == craig_profession:
-        candice_contracts.append(craig_name)
+alfred = homeowner('alfred', 'Alfred Alfredson', ['painter', 'plumber'], 'Alfredslane 123', '-')
+bert = homeowner('bert', 'Bert Bertson', ['plumber'], 'Bertslane 231', '-')
+candice = homeowner('candice', 'Candice Candicedottir', ['electrician', 'painter'], 'Candicelane 312', '-')
+test = homeowner('test', 'Schraal Test Figuur', ['plumber','plumber','plumber','electrician','painter'],'Doos op brak terrein', 'buitenveld')
 
-print("Alfred's contracts:", alfred_contracts)
-print("Bert's contracts:", bert_contracts)
-print("Candice's contracts:", candice_contracts)
+# end of data addition
+# start of logics
+
+def make_match_list(owner, need):
+    for specialist in specialist_list:
+        if globals()[specialist].profession == need:
+            globals()[owner].contract_possibility_list.append({globals()[specialist].name : globals()[specialist].price_per_hour})
 
 
-# current assignment, unfinished
+for owner in homeowner_list:
+    for need in locals()[owner].needs:
+        make_match_list(owner, need)
+
+# end of logics
+# N.B.
+# logic can be added to find the best possible contract, this however requires a lot more data about the specialists.
+# with the amount of data given there is little else to be done then giving the price per hour with the possible contracts.
+# in this case even that information is not there.
+# use .identify() on any instance to see most of their data
+
+# following are a few lines to check/test the code
+for person in specialist_list:
+    locals()[person].identify()
+    print('')
+
+for owner in homeowner_list:
+    locals()[owner].identify()
+    print('')
+    
+# This file has been created as an exercize and should not be considered as serious
