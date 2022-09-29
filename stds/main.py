fbbd96e57f5e3453dbbedb5343fe149c2a26334b
Remi <remi.meijer@gmail.com>
2022-09-29 19:09:48 +0200

    29-09-2022

--- stds/main.py
+++ stds/main.py
@@ -1,31 +1,33 @@
 # Leave these lines untouched
 __winc_id__ = "8c2e6882503c4baa9ce2e050497c3f2f"
 __human_name__ = "stds"
-
 # Your code here
 
 import sys
 
-
 def main():
     linelist = []
+    linelistold = []
     argin1 = []
-    for line in sys.stdin:
-        linelist.append(line) # linelist contains all lines of stdin
     for line in sys.argv:
         argin1.append(line[-1])
-    print(argin1)
     if len(argin1) != 2:
-        print('This program requires precisely one argument of which the last character will be used as definitive argument.')
+        print('This program requires precisely one argument of which the last character will be used as the definitive argument.')
         print('try again with that confinement.')
         sys.exit()
     argument = argin1[1]
-    
-    # TODO: Print the result to stdout
-
-    # TODO: Print the total number of removed characters to stderr
-    ...
-
+    removed_count = 0
+    for line in sys.stdin:
+        temp = []
+        for character in line:
+            if character != argument:
+                temp.append(character)
+            else:
+                removed_count += 1
+        linelist.append(''.join(temp))       
+        linelistold.append(line) # linelist contains all lines of stdin
+    sys.stderr.write(str(removed_count))
+    sys.stdout.write(''.join(linelist))
 
 if __name__ == "__main__":
     main()
